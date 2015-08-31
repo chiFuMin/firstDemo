d00326d2fca0f61af30dc1285246187b372334fc
chifumin <2391692017@qq.com>
2015-08-31 21:15:28 +0800

    第二次！

--- GitHubDemo6/GitHubDemo6/ViewController.m
+++ GitHubDemo6/GitHubDemo6/ViewController.m
@@ -18,10 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)print{
+    NSLog(@"asdf");
 }
+
 
 @end
